2086957818532ce7d45b6f1741ba5ffd7accfffc
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-10-15 14:46:42 -0500

    formulario de login

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms'
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent {
 
+  loginForm : FormGroup
+
+  constructor(private fb:FormBuilder){
+
+    this.loginForm = fb.group({
+      email: ["",[Validators.required,Validators.email]],
+      password: ["",[Validators.required,Validators.minLength(8)]]
+    })
+  }
+
+  tryLogin(){
+    console.log(this.loginForm.value)
+  }
 }
