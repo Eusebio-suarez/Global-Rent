d0b2006955fd087c95e18395a1a3f3c1651a65bd
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-10-16 07:53:21 -0500

    servicio auth

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { ApiResponse } from './models/apiResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  readonly API_BASE_URL="http://localhost:8080/api/v1/auth"
+
+  private http:HttpClient = Inject(HttpClient)
+
+  login(credentials:{email:string,password:string}):Observable<ApiResponse<string>>{
+
+    return this.http.post<ApiResponse<string>>(this.API_BASE_URL+"/login",credentials).pipe(
+      tap(res =>{
+        console.log("login exitoso")
+        localStorage.setItem("Authorization","Bearer "+res.data)
+      }),
+    
+      catchError(error => {
+        const message = error.error?.message||"Error al iniciar Sesión."
+
+        return throwError(() => new Error(message))
+
+        
+      })
+    )
+  }
+}
