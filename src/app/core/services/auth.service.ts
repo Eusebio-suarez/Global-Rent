f1c55bf02766eb0b04432765e408b6d29546badd
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-10-16 10:11:00 -0500

    fueature de login

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { ApiResponse } from '../models/response/apiResponse';
+import { LoginRequest } from '../models/request/loginRequest';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  readonly API_BASE_URL="http://localhost:8080/api/v1/auth"
+
+  private http = inject(HttpClient)
+
+  login(credentials:LoginRequest):Observable<ApiResponse<{token:string}>>{
+
+    return this.http.post<ApiResponse<{token:string}>>(this.API_BASE_URL+"/login",credentials).pipe(
+      tap(res =>{
+        localStorage.setItem("Authorization","Bearer "+res.data.token)
+      }),
+    
+      catchError(error => {
+        const message = error.error?.message||"Error al iniciar Sesión."
+
+        return throwError(() => new Error(message))
+      })
+    )
+  }
+}
--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { NgClass } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth.service';
+import { ApiResponse } from '../../../../core/models/response/apiResponse';
 @Component({
   selector: 'app-login-form',
   imports: [ReactiveFormsModule, RouterLink, NgClass],
   templateUrl: './login-form.component.html'
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+ 
+  loginForm!:FormGroup
 
-  loginForm : FormGroup
+  private fb:FormBuilder = inject(FormBuilder)
+  private authservice:AuthService = inject(AuthService)
 
-  constructor(private fb:FormBuilder){
-
+  ngOnInit() {
     this.loginForm = this.fb.group({
       email: ["",[Validators.required,Validators.email]],
       password: ["",[Validators.required,Validators.minLength(8)]]
@@ -20,6 +24,13 @@ export class LoginFormComponent {
   }
 
   tryLogin(){
-    console.log(this.loginForm.value)
+    this.authservice.login(this.loginForm.value).subscribe({
+      next:(response)=>{
+        console.log("exito")
+      },
+      error:(e:Error)=>{
+        console.log(e)
+      }
+    })
   }
 }
