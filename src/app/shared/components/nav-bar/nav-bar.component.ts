a4932caa79f26935f32698680379a824838c52b4
Eusebio Suarez <eusebiosuaresmartines@gmail.com>
2025-11-11 09:18:33 -0500

    barra de navegacion

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
+
+  showMenu = signal<boolean>(true)
+
+
+  toggle(){
+    this.showMenu.update(value => !value)
+    console.log(this.showMenu())
+  }
 
 }
