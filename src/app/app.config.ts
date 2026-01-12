d2a91fa365a3f1222854fa233bbf6846fb286376
Eusebio Suarez <eusebiosuaresmartines@gmail.com>
2026-01-12 14:31:17 -0500

    configurar interceptor jwt de pasquete externo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(), // required animations providers
     provideToastr(), // Toastr providers
+
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config:{
+          headerName:"Authorization",
+          allowedDomains:["localhost:8080"],
+          tokenGetter:(()=> localStorage.getItem("Authorization")),
+          disallowedRoutes:["/api/v1/auth/login","/api/v1/auth/register"]
+        }
+      })
+    ),
+
+    provideHttpClient(withInterceptorsFromDi())
+
   ]
 };
